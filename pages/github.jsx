493f23ca54f9a891869aeda6f85aef4d79d7f2a7
Mauro Habbaby <mauroh@gmail.com>
2022-03-12 12:50:25 -0300

    Arreglado GitHub Info

--- pages/github.jsx
+++ pages/github.jsx
@@ -69,6 +69,9 @@ export async function getStaticProps() {
     }
   );
   let repos = await repoRes.json();
+  if (!repos) {
+    repos = [];
+  }
   repos = repos
     .sort((a, b) => b.stargazers_count - a.stargazers_count)
     .slice(0, 6);
