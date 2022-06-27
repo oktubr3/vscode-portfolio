dedbce45e1697a880c35f3da7d4a4629b6085c42
oktubr3 <mauroh@gmail.com>
2022-06-27 12:51:51 -0300

    Little Change in About Page

--- pages/about.jsx
+++ pages/about.jsx
@@ -5,14 +5,7 @@ const AboutPage = () => {
     <>
       <h3>A Little Bit About Me</h3>
       <br></br>
-      <p className={styles.line}>I am passionate about technology, I enjoy
-        software development and from
-        teenager I was interested in electronics.
-        I promote and celebrate Open Source, the
-        Free Software and GNU/Linux
-        My goal for the future is to
-        work as a Full Developer
-        Stack, for that I train every day</p>
+      <p className={styles.line}>I am passionate about technology, I enjoy software development and since I was a child I was interested in electronics. I promote and celebrate Open Source, Free Software and GNU/Linux. My goal for the future is to work as a Full Developer Stack, for that I train hard every day.</p>
     </>
   );
 };
