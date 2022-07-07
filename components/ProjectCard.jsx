e833c90c93a716752c2c572b69e41688aea28344
Mauro Habbaby <mauroh@gmail.com>
2022-07-07 01:04:30 -0300

    Link to projects Fixed

--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -1,43 +1,46 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import styles from '../styles/ProjectCard.module.css';
 
 const ProjectCard = ({ project }) => {
-  return (
-    <div className={styles.card}>
-      <Image src={project.image} height={600} width={600} alt={project.name} />
-      <div className={styles.content}>
-        <h3>{project.name}</h3>
-        <p>{project.description}</p>
-        <div className={styles.tags}>
-          {project.tags.map((tag) => (
-            <span key={tag} className={tag}>
-              {tag}
-            </span>
-          ))}
-        </div>
-        <div className={styles.cta}>
-          {project.source_code && (
-            <a
-              href={project.source_code}
-              target="_blank"
-              rel="noopener noreferrer"
-              className={styles.underline}
-            >
-              Source Code
-            </a>
-          )}
-          <a
-            href={project.demo}
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.underline}
-          >
-            Live Demo
-          </a>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className={styles.card}>
+			<a target="_blank" href={project.demo} rel="noopener noreferrer">
+				<Image src={project.image} height={600} width={600} alt={project.name} />
+			</a>
+			<div className={styles.content}>
+				<h3>{project.name}</h3>
+				<p>{project.description}</p>
+				<div className={styles.tags}>
+					{project.tags.map((tag) => (
+						<span key={tag} className={tag}>
+							{tag}
+						</span>
+					))}
+				</div>
+				<div className={styles.cta}>
+					{project.source_code && (
+						<a
+							href={project.source_code}
+							target="_blank"
+							rel="noopener noreferrer"
+							className={styles.underline}
+						>
+							Source Code
+						</a>
+					)}
+					{/* <a
+						href={project.demo}
+						target="_blank"
+						rel="noopener noreferrer"
+						className={styles.underline}
+					>
+						Live Demo
+					</a> */}
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default ProjectCard;
